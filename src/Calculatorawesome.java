b79ea1d23b6e0c4da86d253647031d22edb8bbe2
Mrfuzzy <league@iMac.attlocal.net>
2015-11-08 17:31:57 -0800

    gg
    gg

--- src/Calculatorawesome.java
+++ src/Calculatorawesome.java
@@ -1,21 +1,22 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Calculatorawesome implements ActionListener{
 	JFrame frame;
+	JPanel panel;
 	Calculatorawesome(){
 		frame = new JFrame();
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-	Jpanel
+	
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		
 			
 			
@@ -31,7 +32,7 @@ public class Calculatorawesome implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
