aeba38ed01b7c0eb840f21297b5a6d78ff97f4ae
BigRockerEnding <bigrockerending@live.com>
2015-11-22 17:22:17 -0800

    gg
    gg

--- src/SimonSaysRecipe.java
+++ src/SimonSaysRecipe.java
@@ -16,6 +16,7 @@ public class SimonSaysRecipe extends KeyAdapter {
  
  // Complete steps 1 - 7 before you test
  // 1. Make a JFrame variable 
+	int points = 0;
 	JFrame frame;
  	HashMap<Integer,String> images = new HashMap<Integer, String>();
  	private int imageIndex;
@@ -24,10 +25,10 @@ public class SimonSaysRecipe extends KeyAdapter {
  	Date timeAtStart;
  	private  void makeAlbum() {
  // 2. add 4 images which match keyboard keys like this: images.put(new Integer(KeyEvent.VK_UP), "image.jpg");
-  	images.put(new Integer(KeyEvent.VK_UP), "imgres.jpg");
-  	images.put(new Integer(KeyEvent.VK_LEFT), "blue-button-hi.png");
-  	images.put(new Integer(KeyEvent.VK_DOWN), "images-1.jpg");
-  	images.put(new Integer(KeyEvent.VK_RIGHT), "images.jpg");
+  	images.put(new Integer(KeyEvent.VK_UP), "images-3.jpg");
+  	images.put(new Integer(KeyEvent.VK_LEFT), "imgres.png");
+  	images.put(new Integer(KeyEvent.VK_DOWN), "imgres-1.jpg");
+  	images.put(new Integer(KeyEvent.VK_RIGHT), "images-2.jpg");
  // 3. Tell the user to "Press the matching key when 'Simon says' otherwise press a different key"
   	JOptionPane.showMessageDialog(null, "Press the matching key when 'Simon says' otherwise press a different key");
  // 4. call the method to show an image
@@ -37,17 +38,36 @@ public class SimonSaysRecipe extends KeyAdapter {
 	public void keyPressed(KeyEvent e) {
     	int keyCode = e.getKeyCode();
     	// 16. make a points variable to track the score. tell the user their score at the end.
-    	//17. if the keyCode matches the imageIndex and "Simon says..."  increase their score
-    	//18.   if the keyCode doesn't match the imageIndex and "Simon didn't say..."  increase their score	
-    	//19. Use the speak method to tell the user if they were correct or not
-    	//13. increment tries by 1
-    	showImage();
-    	//14. if tries is greater than 9 (or however many you want)
     	
+    	//17. if the keyCode matches the imageIndex and "Simon says..."  increase their score
+	if(keyCode == imageIndex && simonSays == 0 )
+    	{
+    		points++;
+    		speak("correct"); 
+    	}
+    	//18.   if the keyCode doesn't match the imageIndex and "Simon didn't say..."  increase their score	
+	else if( keyCode != imageIndex && simonSays != 0)
+	{
+		points++;
+		speak("correct"); 
+	}
+    	//19. Use the speak method to tell the user if they were correct or not
+		
+			
+		
+    	//13. increment tries by 1
+    	 tries++;
+    	//14. if tries is greater than 9 (or however many you want)
+    	if(tries > 9)
+    	{
+    		JOptionPane.showMessageDialog(null, points);
+    		System.exit(0);	
+    	}
     	//15.    	exit the program
     	//11. dispose of the frame
     	frame.dispose();
     	//12. call the method to show an image
+    	showImage();
 	}			
 	private void showImage() {
     	//5. initialize your frame to a new JFrame()
@@ -65,7 +85,7 @@ public class SimonSaysRecipe extends KeyAdapter {
    simonSays =  new Random().nextInt(2);
      	if (simonSays == 0)
      	{
-     		speak("Simon says press this key");
+     		speak( " Simon says press this key ");
      		
      	}
      	else{
@@ -96,7 +116,7 @@ public class SimonSaysRecipe extends KeyAdapter {
 /* 
 * 20. add a timer
 * ~~~ where the code starts running ~~~
-* timeAtStart = new Date();
+timeAtStart = new Date();
 *
 * ~~~ where the code ends ~~~
 * Date timeAtEnd = new Date();
---
+++ src/Virtual_Dice.java
@@ -0,0 +1,9 @@
+
+public class Virtual_Dice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
