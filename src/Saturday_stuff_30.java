ea85b49d9febe4fc3befe01c9c89c0dc362ac378
BigRockerEnding <bigrockerending@live.com>
2016-01-30 12:04:38 -0800

    gg
    gg

---
+++ src/Saturday_stuff_30.java
@@ -0,0 +1,32 @@
+public class Saturday_stuff_30 {
+
+	public static void main(String[] args) {
+		Saturday_stuff_30 b = new Saturday_stuff_30();
+
+	}
+
+	Saturday_stuff_30() {
+		getTheBestCoder("Branodn");
+	}
+//	repeat("hello", 5);
+	// echo("hfghjhghjghhjgb");
+		// }
+
+		 void echo(String f){
+	 System.out.println(f + " "+ f);
+		 }
+	//void repeat(String g, int x) {
+	//	System.out.println();
+	//	for (int i = 0; i < x; i++) {
+		//	System.out.println("hello");
+	//	}
+	//}
+	String getTheBestCoder(String x){
+		System.out.println(x);
+		return x;
+		
+		
+		
+	}
+}
+
