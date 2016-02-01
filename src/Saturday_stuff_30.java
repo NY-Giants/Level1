be7ab23750388a9d8530f3538c8f68ba6ca8cc13
Mrfuzzy <league@WTS9.attlocal.net>
2016-01-31 17:25:40 -0800

    gg
    gg

--- src/Saturday_stuff_30.java
+++ src/Saturday_stuff_30.java
@@ -2,31 +2,47 @@ public class Saturday_stuff_30 {
 
 	public static void main(String[] args) {
 		Saturday_stuff_30 b = new Saturday_stuff_30();
-
 	}
 
 	Saturday_stuff_30() {
-		getTheBestCoder("Branodn");
+		
+		// getTheBestCoder("Branodn");
+
+		 boolean waffles = odd(66);
+		 System.out.println(waffles);
 	}
-//	repeat("hello", 5);
+	// repeat("hello", 5);
 	// echo("hfghjhghjghhjgb");
-		// }
+	// }
 
-		 void echo(String f){
-	 System.out.println(f + " "+ f);
-		 }
-	//void repeat(String g, int x) {
-	//	System.out.println();
-	//	for (int i = 0; i < x; i++) {
-		//	System.out.println("hello");
-	//	}
-	//}
-	String getTheBestCoder(String x){
-		System.out.println(x);
-		return x;
-		
-		
-		
+	// void echo(String f){
+	// System.out.println(f + " "+ f);
+	// }
+	// void repeat(String g, int x) {
+	// System.out.println();
+	// for (int i = 0; i < x; i++) {
+	// System.out.println("hello");
+	// }
+	// }
+	// String getTheBestCoder(String x){
+	// System.out.println(x);
+	// return x;
+
+	// }
+	// int add(int x, int z) {
+	// int sum = x + z;
+	// return sum;
+
+	// }
+	boolean odd(int x) {
+		if (x % 2 == 0) {
+			return false;
+			
+		} 
+		else {
+			
+			return true;
+		}
 	}
-}
 
+}
