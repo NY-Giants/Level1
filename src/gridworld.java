54dfd40701ad38bd39402be8d8695de113ae3842
Revanty <league@WTS14.attlocal.net>
2016-05-08 17:30:02 -0700

    gg
    gg

---
+++ src/gridworld.java
@@ -0,0 +1,102 @@
+import info.gridworld.actor.Bug;
+import info.gridworld.grid.Location;
+import info.gridworld.world.World;
+
+public class gridworld {
+	public static void main(String[] args) {
+		World world = new World();
+		world.show();
+		// Bug bug = new Bug();
+		// Bug insect = new Bug();
+		// Location location = new Location(2, 1);
+		// world.add(location, bug);
+		// Location Area_51 = new Location(2, 2);
+		// .add(Area_51, insect);
+		// insect.setColor(Color.cyan);
+		// insect.turn();
+		// insect.turn();
+
+		// Flower flowa = new Flower();
+		// LOCation = new Location(2, 0);
+		// world.add(LOCation, flowa);
+		// Location LOCAtion = new Location(2, 3);
+		// world.add(LOCAtion, flower);
+		// for (int y = 0; y < 10; y++) {
+		// for (int x = 0; x < 10; x++) {
+		// System.out.println(x + "" + y);
+		// if (x % 2 == 0) {
+		// Flower flower = new Flower();
+		// flower.setColor(Color.BLUE);
+		// world.add(new Location(x, y), flower);
+		// } else {
+		// Flower flower = new Flower();
+		// flower.setColor(Color.RED);
+		// world.add(new Location(x, y), flower);
+		// }
+
+		// }
+
+		// }
+		Bug bug1 = new Bug();
+		Bug bug2 = new Bug();
+		Bug bug3 = new Bug();
+		Bug bug4 = new Bug();
+		Bug bug5 = new Bug();
+		Bug bug6 = new Bug();
+		Bug bug7 = new Bug();
+		Bug bug8 = new Bug();
+		Bug bug9 = new Bug();
+		Bug bug10 = new Bug();
+		Bug bug11 = new Bug();
+		Bug bug12 = new Bug();
+		Bug bug13 = new Bug();
+		Bug bug14 = new Bug();
+		Bug bug15 = new Bug();
+		Bug bug16 = new Bug();
+		Bug bug17 = new Bug();
+		Bug bug18 = new Bug();
+		Bug bug19 = new Bug();
+		Bug bug20 = new Bug();
+
+		Location location1 = new Location(1, 1);
+		Location location2 = new Location(1, 9);
+		Location location3 = new Location(1, 1);
+		Location location4 = new Location(1, 1);
+		Location location5 = new Location(1, 1);
+		Location location6 = new Location(1, 1);
+		Location location7 = new Location(1, 1);
+		Location location8 = new Location(1, 1);
+		Location location9 = new Location(1, 1);
+		Location location10 = new Location(1, 1);
+		Location location11 = new Location(1, 1);
+		Location location12 = new Location(1, 1);
+		Location location13 = new Location(1, 1);
+		Location location14 = new Location(1, 1);
+		Location location15 = new Location(1, 1);
+		Location location16 = new Location(1, 1);
+		Location location17 = new Location(1, 1);
+		Location location18 = new Location(1, 1);
+		Location location19 = new Location(1, 1);
+		world.add(location1, bug1);
+		world.add(location2, bug2);
+		world.add(location3, bug3);
+		world.add(location4, bug4);
+		world.add(location5, bug5);
+		world.add(location6, bug6);
+		world.add(location7, bug7);
+		world.add(location8, bug8);
+		world.add(location9, bug9);
+		world.add(location10, bug10);
+		world.add(location11, bug11);
+		world.add(location12, bug12);
+		world.add(location13, bug13);
+		world.add(location14, bug14);
+		world.add(location15, bug15);
+		world.add(location16, bug16);
+		world.add(location17, bug17);
+		world.add(location18, bug18);
+		world.add(location19, bug19);
+
+	}
+
+}
