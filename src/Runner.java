e4b5f0f2083fe9124b546da39cad4f41ad2273b2
Wintress Mac <DON'T CHANGE THIS>
2016-07-17 17:28:33 -0700

    gg
    gg

---
+++ src/Runner.java
@@ -0,0 +1,30 @@
+
+public class Runner {
+	
+	public static void main(String[] args) {
+		theInternet izterrible = new theInternet();
+		izterrible.getOffened();
+		izterrible.becomeFirstComment();
+		izterrible.adRevenue();
+		System.out.println(999999999);
+		GeniusOrNot Iamgenius = new GeniusOrNot();
+		Iamgenius.Question();
+		Iamgenius.GetQuestion();
+		Iamgenius.IfRight();
+		
+		The_ESPYS x = new The_ESPYS();
+		x.DecideWhoIsHost();
+		x.GetNumberOfNotablePeopleRetiringThisYear();
+		System.out.println(3);
+		x.WasJoeBidenThere();
+		System.out.println(true);
+		
+		Mai_naem_is_jeff jeff = new Mai_naem_is_jeff();
+		jeff.saysumtinfunney();
+		jeff.Money_amoundts();
+		System.out.println(1);
+		jeff.joekasabroek();
+		System.out.println(true);
+	}
+
+}
