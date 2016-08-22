21f43594f41be6e05b3b88dd106c6890f0c3f1d8
Love4653 <league@WTS5.attlocal.net>
2016-08-21 17:08:33 -0700

    gg
    gg

--- src/OddEven.java
+++ src/OddEven.java
@@ -1,12 +1,20 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class OddEven {
+public class OddEven implements ActionListener {
 	JFrame frame;
 	JPanel panel;
-	JButton button;
-	JButton button2;
+	JButton odd;
+	JButton even;
+	JLabel label;
+	JLabel label2;
+	Random R = new Random();
 
 	public static void main(String[] args) {
 		OddEven oe = new OddEven();
@@ -16,14 +24,40 @@ public class OddEven {
 	OddEven() {
 		frame = new JFrame();
 		panel = new JPanel();
-		button = new JButton("Odd");
-		button2 = new JButton("Even");
+		odd = new JButton("Odd");
+		even = new JButton("Even");
+		label = new JLabel();
+		label2 = new JLabel();
 		frame.add(panel);
-		panel.add(button);
-		panel.add(button2);
+		panel.add(odd);
+		panel.add(even);
+		panel.add(label);
+		panel.add(label2);
 		frame.setVisible(true);
 		frame.setSize(500, 500);
+		odd.addActionListener(this);
+		even.addActionListener(this);
+		label.setVisible(true);
 
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == odd) {
+
+			int rand = R.nextInt(1010);
+			rand *= 2;
+			rand++;
+			label.setText(rand + "");
+
+		}
+		if (e.getSource() == even) {
+
+			int rando = R.nextInt(1010);
+			rando*=2;
+			label.setText(rando + "");
+			
+		}
 	}
 
 }
